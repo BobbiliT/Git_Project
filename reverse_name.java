995920cf3a0ae392f8dc3cf8f32084ee86f07877
tejesh <bobbilitejeswara42@gmail.com>
2025-06-07 08:25:31 +0530

    add new changes

---
+++ reverse_name.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class reverse_name {
+    public static void main(String[] args) {
+        System.out.println("enter your name ");
+        Scanner s = new Scanner(System.in);
+        String name = s.nextLine();
+        String rev = "";
+        for(int i=name.length()-1; i>=0; i--){
+            rev = rev+name.charAt(i);
+        }
+        System.out.println(rev);
+    }
+}
