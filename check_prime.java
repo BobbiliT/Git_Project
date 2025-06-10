1859666b2e348076795da78291442de8ab536a8e
tejesh <bobbilitejeswara42@gmail.com>
2025-06-10 14:33:42 +0530

    add new changes

---
+++ check_prime.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class check_prime {
+    public static void main(String[] args) {
+        System.out.println("enter your number ");
+        Scanner s = new Scanner(System.in);
+        int num = s.nextInt();
+        int count = 0;
+        if(num>1){
+            
+        }
+        else{
+            System.out.println("is not prime.");
+        }
+    }
+}
